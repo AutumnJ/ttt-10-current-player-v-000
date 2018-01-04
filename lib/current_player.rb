adf62fb0fce5dc6744a03ac79dd8c911cfeee379
Autumn Jung <autumnkangas@gmail.com>
2018-01-04 18:25:53 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  counter = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      counter += 1
+      return counter
+    end
+  end
+end
+
+def current_player
+
+def method_name
+  
+end
